1715d96ee758e3013afe4e8ad1f342510c8bd894
OpYi-AI <test@aizonlabs.com>
2025-06-10 15:25:29 +0200

    Updated build-manifest.json

--- startuplandingsimple/.next/static/chunks/webpack.js
+++ startuplandingsimple/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a91a366253e06107"; }
+/******/ 		__webpack_require__.h = function() { return "0755bb408f5e1cc4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
